86e77d972c18b3a3212e8523d488d2f45ece1718
kishi sakuya <sky.skyks@gmail.com>
2021-04-01 00:16:01 +0900

    add1

--- Assets/BasketController.cs
+++ Assets/BasketController.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class BasketController : MonoBehaviour
 {
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource aud;
+    GameObject diector;
+
+    void Start() {
+        this.diector = GameObject.Find("GameDirector");
+        this.aud = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other) {//colliderが引数に渡される
+        if (other.gameObject.tag == "Apple") {
+            this.diector.GetComponent<GameDirector>().GetApple();
+            this.aud.PlayOneShot(this.appleSE);
+        } else {
+            this.diector.GetComponent<GameDirector>().GetBomb();
+            this.aud.PlayOneShot(this.bombSE);
+        }
+        Destroy(other.gameObject); //colliderがついているオブジェクトを消す
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject timerText;
+    GameObject pointText;
+    float time = 60.0f;
+    int point = 0;
+
+    public void GetApple() {
+        this.point += 100;
+    }
+    public void GetBomb() {
+        this.point /= 2;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.timerText = GameObject.Find("Time");
+        this.pointText = GameObject.Find("Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.time -= Time.deltaTime;
+        this.timerText.GetComponent<Text>().text = this.time.ToString("F1");
+        this.pointText.GetComponent<Text>().text = this.point.ToString() + "point";
+    }
+}
