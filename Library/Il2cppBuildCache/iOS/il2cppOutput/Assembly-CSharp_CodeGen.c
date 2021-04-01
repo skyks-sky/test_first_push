3f9b83728021e78c1f27c3a0e8f031871f57cd82
kishi sakuya <sky.skyks@gmail.com>
2021-04-01 22:50:25 +0900

    add2

---
+++ Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,99 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void BasketController::Start()
+extern void BasketController_Start_m2785584B45FC2BAE13427C9885B7F9C55F00A4A7 (void);
+// 0x00000002 System.Void BasketController::OnTriggerEnter(UnityEngine.Collider)
+extern void BasketController_OnTriggerEnter_m0BCD5FA46E4A4297B5E6CB1391185E3BD2AC74BB (void);
+// 0x00000003 System.Void BasketController::Update()
+extern void BasketController_Update_mCF7DA1E3C9D834F94DADE8C1102D3CE23514475C (void);
+// 0x00000004 System.Void BasketController::.ctor()
+extern void BasketController__ctor_m442E38D5A38EBA2851FDA724E59F676DDCBAD99E (void);
+// 0x00000005 System.Void GameDirector::GetApple()
+extern void GameDirector_GetApple_mD3C77014A99EF65083FB343E2DD3C6ECB060F497 (void);
+// 0x00000006 System.Void GameDirector::GetBomb()
+extern void GameDirector_GetBomb_m33566B2B35BAF8800207C8A664F5EC71CE574578 (void);
+// 0x00000007 System.Void GameDirector::Start()
+extern void GameDirector_Start_m92DAF8BCF048D1F77B51F51BCD4CA47D260F6DC9 (void);
+// 0x00000008 System.Void GameDirector::Update()
+extern void GameDirector_Update_m619B224507888150A939D1E0917CA9F27BDB090D (void);
+// 0x00000009 System.Void GameDirector::.ctor()
+extern void GameDirector__ctor_m806FADCD19DEA27E4920565E5FF62352956A937C (void);
+// 0x0000000A System.Void ItemController::Update()
+extern void ItemController_Update_mE36851F099595C99F2B073D240EE1B0945E79A0E (void);
+// 0x0000000B System.Void ItemController::.ctor()
+extern void ItemController__ctor_m5EB571A3AD3B0DA80AAE5DE040E4C841B6AA99AB (void);
+// 0x0000000C System.Void itemGenerator::SetParameter(System.Single,System.Single,System.Int32)
+extern void itemGenerator_SetParameter_mB716CA7C875017C5ED9CD6942195E524D51E1931 (void);
+// 0x0000000D System.Void itemGenerator::Update()
+extern void itemGenerator_Update_m715ED9D41ED5FD92947933F66D97AEC2D94C7140 (void);
+// 0x0000000E System.Void itemGenerator::.ctor()
+extern void itemGenerator__ctor_m5DBD46E497232D4655F1EE00102B9D77B3059B61 (void);
+static Il2CppMethodPointer s_methodPointers[14] = 
+{
+	BasketController_Start_m2785584B45FC2BAE13427C9885B7F9C55F00A4A7,
+	BasketController_OnTriggerEnter_m0BCD5FA46E4A4297B5E6CB1391185E3BD2AC74BB,
+	BasketController_Update_mCF7DA1E3C9D834F94DADE8C1102D3CE23514475C,
+	BasketController__ctor_m442E38D5A38EBA2851FDA724E59F676DDCBAD99E,
+	GameDirector_GetApple_mD3C77014A99EF65083FB343E2DD3C6ECB060F497,
+	GameDirector_GetBomb_m33566B2B35BAF8800207C8A664F5EC71CE574578,
+	GameDirector_Start_m92DAF8BCF048D1F77B51F51BCD4CA47D260F6DC9,
+	GameDirector_Update_m619B224507888150A939D1E0917CA9F27BDB090D,
+	GameDirector__ctor_m806FADCD19DEA27E4920565E5FF62352956A937C,
+	ItemController_Update_mE36851F099595C99F2B073D240EE1B0945E79A0E,
+	ItemController__ctor_m5EB571A3AD3B0DA80AAE5DE040E4C841B6AA99AB,
+	itemGenerator_SetParameter_mB716CA7C875017C5ED9CD6942195E524D51E1931,
+	itemGenerator_Update_m715ED9D41ED5FD92947933F66D97AEC2D94C7140,
+	itemGenerator__ctor_m5DBD46E497232D4655F1EE00102B9D77B3059B61,
+};
+static const int32_t s_InvokerIndices[14] = 
+{
+	1121,
+	961,
+	1121,
+	1121,
+	1121,
+	1121,
+	1121,
+	1121,
+	1121,
+	1121,
+	1121,
+	421,
+	1121,
+	1121,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	14,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
